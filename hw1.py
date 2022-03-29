efac740e4ec47b3896bf328e7330cb15d58f88d1
John Hahn <70570472+john-hahn@users.noreply.github.com>
2022-03-30 06:29:09 +0900

    Added hw1 file

---
+++ hw1.py
@@ -0,0 +1,46 @@
+# Paragon National Group Spring 2022
+# Computer Science / Data Science Track
+# HW 1
+
+# Assigned: March 29, 2022
+# Due: April 5, 2022 6:00pm
+
+# This is the first programming assignment of the computer science track.
+# The questions will cover the topics learned in the first lecture, which entailed
+# print statements, various data types, variables, basic operations, and conditionals.
+
+# This function prints out a sentence saying "Welcome to the start of the PNG CS
+# track"
+def print_welcome_message():
+    print('TODO')
+
+# This function takes the number of cents as an argument and prints the best 
+# combination of quarters, dimes, nickels, and pennies in descending order of
+# importance (i.e. 25 cents can be split into 25 pennies, but quarters is more
+# important, thus it would be split into 1 quarter). Assume that the argument given
+# is non-negative.
+# Ex) split_change(1) -> 0 quarters, 0 dimes, 0 nickels, 1 penny
+#     split_change(20) -> 0 quarters, 2 dimes, 0 nickels, 0 pennies
+#     split_change(67) -> 2 quarters, 1 dime, 1 nickel, 2 pennies 
+def split_change(cents):
+    print('TODO')
+
+# This function takes two strings as arguments and returns the concatenated
+# string. Assume that the strings provided do not have trailing or leading 
+# whitespaces. This means you have to add the whitespace in between the two
+# strings yourself. The code MUST utilize variables. 
+# Ex) concatenate_strings('I love', 'coding!') -> 'I love coding!'
+#     concatenate_strings('Paragon', 'National Group') -> 'Paragon National Group'
+def concatenate_strings(s1, s2):
+    #TODO
+    return
+
+# This function takes five strings (the strings can only either be 'H', 'h', 'T'
+# or 't') as arguments and returns the number of heads as an integer. Assume that
+# no other strings can be inputted other than those four types.
+# Ex) number_of_heads('H', 'h', 'H', 'H', 'h') -> 5
+#     number_of_heads('H', 'T', 'H', 't', 'H') -> 3
+#     number_of_heads('T', 't', 'h', 'h', 'H') -> 3
+def number_of_heads(s1, s2, s3, s4, s5):
+    #TODO
+    return
